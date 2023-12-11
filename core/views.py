82905dedffd5a3a61ae6dfa12f65fa261ed5be58
Davi Augusto <111746350+davigaldino@users.noreply.github.com>
2023-12-10 21:00:35 -0300

    Commit da alteração para efetuar a soma, multiplicação, subtração e divisão de dois numeros

--- core/views.py
+++ core/views.py
@@ -4,4 +4,8 @@ from django.shortcuts import render, HttpResponse
 
 def hello(request, nome, idade, numero1, numero2):
     soma = numero1 + numero2
-    return HttpResponse('<h1>Hello {} de {} anos</h1><p></p> <h3>A soma é igual a {}</h3>'.format(nome, idade, soma))
+    mult = numero1 * numero2
+    divisao = numero1 / numero2
+    sub = numero1 - numero2
+    return HttpResponse('<h1>Hello {} de {} anos</h1><p></p> <h3>A soma é igual a {}</h3><h3>A multiplicação é igual a {}</h3>'
+                        '<h3>A divisão é igual a {}</h3><h3>A subtração é igual a {}</h3>'.format(nome, idade, soma, mult, divisao, sub))
